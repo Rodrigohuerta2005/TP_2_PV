73b118bc093c251a2b7c5ada897e81c059b2c542
rodri <rodri@Rodri>
2024-04-28 18:15:10 -0300

    creacion de las categorias : origen de fabricacion y categoria

--- src/ar/edu/unju/fi/ejercicio1/model/Producto.java
+++ src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -1,5 +1,72 @@
 package ar.edu.unju.fi.ejercicio1.model;
 
 public class Producto {
+	private String codigo;
+	private String descripcion;
+	private Double precio_Unitario;
+	private OrigenFabricacion origenFabricacion;
+	private Categoria categoria;
+	
+		public enum OrigenFabricacion {
+			ARGENTINA,
+			CHINA,
+			BRASIL,
+			URUGUAY;
+	    }
+		public enum Categoria {
+			TELEFONIA,
+			INFORMATICA,
+			ELECTROHOGAR,
+			HERRAMIENTAS;
+		}
+		public Producto () {}
+		
+		public Producto(String codigo, String descripcion, Double precio_Unitario, OrigenFabricacion origenFabricacion,
+				Categoria categoria) {
+			super();
+			this.codigo = codigo;
+			this.descripcion = descripcion;
+			this.precio_Unitario = precio_Unitario;
+			this.origenFabricacion = origenFabricacion;
+			this.categoria = categoria;
+		}
+
+		public String getCodigo() {
+			return codigo;
+		}
+		public void setCodigo(String codigo) {
+			this.codigo = codigo;
+		}
+		public String getDescripcion() {
+			return descripcion;
+		}
+		public void setDescripcion(String descripcion) {
+			this.descripcion = descripcion;
+		}
+		public Double getPrecio_Unitario() {
+			return precio_Unitario;
+		}
+		public void setPrecio_Unitario(Double precio_Unitario) {
+			this.precio_Unitario = precio_Unitario;
+		}
+		public OrigenFabricacion getOrigenFabricacion() {
+			return origenFabricacion;
+		}
+		public void setOrigenFabricacion(OrigenFabricacion origenFabricacion) {
+			this.origenFabricacion = origenFabricacion;
+		}
+		public Categoria getCategoria() {
+			return categoria;
+		}
+		public void setCategoria(Categoria categoria) {
+			this.categoria = categoria;
+		}
+		
+
+
+
+
+
 
 }
+
